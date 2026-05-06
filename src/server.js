495489c2a55b47205bb348f81e977a5824fbb5c3
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-05-06 23:33:14 +0200

    add routes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import pino from "pino-http";
+
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from "./middleware/errorHandler.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
@@ -22,36 +22,20 @@ app.use(cors({
   }));
 app.use(express.json());
 app.use(helmet());
-app.use(pino());
+app.use(logger);
 
 app.use(notesRoutes);
-
-app.use(notFoundHandler);
-app.use(errorHandler);
-
-// test route
 
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
 });
 
-app.get("/notes", (req, res) => {
-  res.status(200).json({
-    message: "Retrieved all notes",
-  });
-});
-
-app.get("/notes/:noteId", (req, res) => {
-  const { noteId } = req.params;
-
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
-
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 // підключення до MongoDB
 
